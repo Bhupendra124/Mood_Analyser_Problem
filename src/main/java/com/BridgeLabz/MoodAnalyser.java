815711f1ed8ab8c14d64639454993f614c3675ff
Bhupendra124 <wadekar.bhupendra6@gmail.com>
2021-08-04 15:46:51 +0530

    BHUPENDRA:ADD: mood analyser contain happy or sad message exception handle

--- src/main/java/com/BridgeLabz/MoodAnalyser.java
+++ src/main/java/com/BridgeLabz/MoodAnalyser.java
@@ -1,13 +1,24 @@
 package com.BridgeLabz;
 
 public class MoodAnalyser {
-
-    public String analyseMood(String message) {
-        if (message.contains("SAD"))
-        return "SAD";
-        else
-            return "HAPPY";
+    public String message;
+    public MoodAnalyser(String message){
+        this.message=message;
     }
 
+    public String analyseMood(String message) throws MoodAnalyserException {
+        try {
 
+
+            if (message.isEmpty())
+                throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.ENTERED_EMPTY,"Please type any message");
+            if (message.contains("SAD"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }catch (NullPointerException e){
+            throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.ENTERED_NULL,"Please enter message");
+        }
+
+    }
 }
