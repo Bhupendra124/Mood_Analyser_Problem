0873de3a1ca6a854f186be33f3650017d21cab4a
Bhupendra124 <wadekar.bhupendra6@gmail.com>
2021-08-06 04:59:27 +0530

    ADD:Exception handling

--- src/test/java/com/BridgeLabz/MoodAnalyserTest.java
+++ src/test/java/com/BridgeLabz/MoodAnalyserTest.java
@@ -4,19 +4,28 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
+
     @Test
-    void givenMessage_whenSad_shouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
+ public   void givenMessage_whenSad_shouldReturnSad() throws MoodAnalyserException {
+        try {
+
+
+        MoodAnalyser moodAnalyser=new MoodAnalyser(null);
             String mood = moodAnalyser.analyseMood("This is a Sad Message ");
         Assertions.assertEquals("SAD" ,mood);
     }
-
+  catch (Exception e){
+      System.out.println("Please type message");}
+    }
 
     @Test
-    void givenMessage_WhenNotSad_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser   = new MoodAnalyser();
+    void givenMessage_WhenNotSad_ShouldReturnHappy() throws MoodAnalyserException {
+        try{
+        MoodAnalyser moodAnalyser   = new MoodAnalyser(null);
             String mood =moodAnalyser.analyseMood("This is a Happy Message ");
             Assertions.assertEquals("HAPPY",mood);
 
-    }
+    }catch (Exception e){
+            System.out.println("Please type valid message ");
+        }
 }
